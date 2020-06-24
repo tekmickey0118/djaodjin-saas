tekmickey0118/djaodjin-saas

commit c23245564c99d3aca69438d69f68bc4339b8c7b1

relies on API to set processor key in Vue checkout
additionals:
- consistently use 'detail' to return user-friendly messages
- edits for OpenAPI documentation

saas/api/coupons.py
@@ -55,6 +55,7 @@ class CouponSerializer(serializers.ModelSerializer):
         fields = ('code', 'discount_type', 'discount_value',
             'created_at', 'ends_at', 'description',
             'nb_attempts', 'plan')
+        read_only_fields = ('code',)
 
 
 class SmartCouponListMixin(object):
@@ -235,6 +236,7 @@ class CouponDetailAPIView(CouponMixin, RetrieveUpdateDestroyAPIView):
         .. code-block:: json
 
             {
+                "code": "DIS100",
                 "discount_type": "percentage",
                 "amount": 10000,
                 "ends_at": null,

saas/api/serializers.py
@@ -244,10 +244,10 @@ class TableSerializer(NoModelSerializer):
 
 class MetricsSerializer(NoModelSerializer):
 
-    scale = serializers.FloatField(
+    scale = serializers.FloatField(required=False,
         help_text=_("The scale of the number reported in the tables (ex: 1000"\
         " when numbers are reported in thousands of dollars)"))
-    unit = serializers.CharField(
+    unit = serializers.CharField(required=False,
         help_text=_("Three-letter ISO 4217 code for currency unit (ex: usd)"))
     title = serializers.CharField(
         help_text=_("Title for the table"))
@@ -330,6 +330,8 @@ class OrganizationDetailSerializer(OrganizationSerializer):
         help_text=_("Zip/Postal code"))
     country = CountryField(required=False, allow_blank=True,
         help_text=_("Country"))
+    is_bulk_buyer = serializers.BooleanField(required=False, default=False,
+        help_text=_("Enable GroupBuy"))
     extra = serializers.CharField(required=False, allow_null=True,
         help_text=_("Extra meta data (can be stringify JSON)"))
 
@@ -408,6 +410,8 @@ class WithSubscriptionSerializer(serializers.ModelSerializer):
 
 class OrganizationWithSubscriptionsSerializer(OrganizationDetailSerializer):
 
+    is_bulk_buyer = serializers.BooleanField(required=False, default=False,
+        help_text=_("Enable GroupBuy"))
     subscriptions = WithSubscriptionSerializer(
         source='subscription_set', many=True, read_only=True)
 
@@ -465,8 +469,7 @@ class PlanSerializer(serializers.ModelSerializer):
     renewal_type = EnumField(choices=Plan.RENEWAL_CHOICES, required=False,
         help_text=_("Natural period for the subscription"))
     app_url = serializers.SerializerMethodField()
-    organization = serializers.SlugRelatedField(
-        read_only=True, slug_field='slug',
+    organization = OrganizationSerializer(read_only=True,
         help_text=_("Provider of the plan"))
     skip_optin_on_grant = serializers.BooleanField(required=False,
         help_text=_("True when a subscriber can automatically be subscribed"\

saas/api/backend.py
@@ -31,7 +31,7 @@ from rest_framework.generics import (RetrieveAPIView,
 from rest_framework.response import Response
 
 from ..backends import ProcessorError
-from ..docs import swagger_auto_schema, OpenAPIResponse
+from ..docs import swagger_auto_schema
 from ..mixins import OrganizationMixin
 from .serializers import (BankSerializer, CardSerializer,
     CardTokenSerializer)
@@ -50,6 +50,8 @@ class RetrieveBankAPIView(OrganizationMixin, RetrieveAPIView):
     This API does not trigger payment of a subscriber to a provider. Checkout
     of a subscription cart is done either through the
     :ref:`HTML page<pages_cart>` or :ref:`API end point<api_checkout>`.
+
+    **Tags**: billing
 
     **Examples**
 
@@ -84,6 +86,8 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
     Pass through to the processor to retrieve some details about
     the payment method (ex: credit card) associated to a subscriber.
 
+    **Tags**: billing
+
     **Examples**
 
     .. code-block:: http
@@ -108,6 +112,8 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
         Pass through to the processor to remove the payment method (ex: credit
         card) associated to a subscriber.
 
+        **Tags**: billing
+
         **Examples**
 
         .. code-block:: http
@@ -117,14 +123,15 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
         return super(PaymentMethodDetailAPIView, self).delete(
             request, *args, **kwargs)
 
-    @swagger_auto_schema(request_boby=CardTokenSerializer, responses={
-        200: OpenAPIResponse("", CardSerializer)})
+    @swagger_auto_schema(request_body=CardTokenSerializer)
     def put(self, request, *args, **kwargs):
         """
         Updates a payment method
 
         Pass through to the processor to update some details about
         the payment method (ex: credit card) associated to a subscriber.
+
+        **Tags**: billing
 
         **Examples**
 

saas/api/balances.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, DjaoDjin inc.
+# Copyright (c) 2020, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -31,6 +31,7 @@ from rest_framework.generics import (get_object_or_404,
 from rest_framework.response import Response
 
 from .. import settings
+from ..docs import swagger_auto_schema, OpenAPIResponse
 from ..managers.metrics import abs_monthly_balances, monthly_balances
 from ..models import BalanceLine
 from ..filters import DateRangeFilter
@@ -185,6 +186,8 @@ class BalanceLineListAPIView(ListCreateAPIView):
         return super(BalanceLineListAPIView, self).post(
             request, *args, **kwargs)
 
+    @swagger_auto_schema(responses={
+        200: OpenAPIResponse("success", BalanceLineSerializer)})
     def patch(self, request, *args, **kwargs):
         """
         Updates the order in which lines are displayed
@@ -212,10 +215,18 @@ class BalanceLineListAPIView(ListCreateAPIView):
 
         .. code-block:: json
 
-            [{
-              "newpos": 1,
-              "oldpos": 3
-            }]
+            {
+                "count": 1,
+                "next": null,
+                "previous": null,
+                "results": [
+                    {
+                        "title": "Sales",
+                        "selector": "Receivable",
+                        "rank": 1
+                    }
+                ]
+            }
         """
         with transaction.atomic():
             for move in request.data:

saas/api/billing.py
@@ -65,7 +65,7 @@ class OrganizationCartSerializer(NoModelSerializer):
     """
     Items which will be charged on an order checkout action.
     """
-    items = InvoicableSerializer(many=True)
+    results = InvoicableSerializer(many=True)
 
 
 class CheckoutItemSerializer(NoModelSerializer):
@@ -384,7 +384,7 @@ class CouponRedeemAPIView(GenericAPIView):
     .. code-block:: json
 
         {
-            "details": "Coupon 'LABORDAY' was successfully applied."
+            "detail": "Coupon 'LABORDAY' was successfully applied."
         }
     """
     serializer_class = RedeemCouponSerializer
@@ -399,7 +399,7 @@ class CouponRedeemAPIView(GenericAPIView):
         if serializer.is_valid():
             coupon_code = serializer.data['code']
             if CartItem.objects.redeem(request.user, coupon_code):
-                details = {"details": (
+                details = {'detail': (
                     _("Coupon '%(code)s' was successfully applied.") % {
                         'code': coupon_code})}
                 headers = {}
@@ -408,10 +408,10 @@ class CouponRedeemAPIView(GenericAPIView):
                 # Since we rely on the message to appear after reload of
                 # the cart page in the casperjs tests, we can't get rid
                 # of this statement just yet.
-                messages.success(request._request, details['details'])#pylint: disable=protected-access
+                messages.success(request._request, details['detail'])#pylint: disable=protected-access
                 return Response(details, status=status.HTTP_200_OK,
                                 headers=headers)
-            details = {"details": (
+            details = {'detail': (
                 _("No items can be discounted using this coupon: %(code)s.") % {
                 'code': coupon_code})}
             return Response(details, status=status.HTTP_400_BAD_REQUEST)
@@ -441,20 +441,21 @@ class CheckoutAPIView(CartMixin, OrganizationMixin,
 
     .. code-block:: json
 
-        {"items":
-        [{
-          "subscription":{
+        {
+          "results": [
+          {
+            "subscription": {
               "created_at":"2016-06-21T23:24:09.242925Z",
               "ends_at":"2016-10-21T23:24:09.229768Z",
               "description":null,
-              "organization":{
+              "organization": {
                   "slug":"xia",
                   "full_name":"Xia",
                   "printable_name":"Xia",
                   "created_at":"2012-08-14T23:16:55Z",
                   "email":"xia@localhost.localdomain"
               },
-              "plan":{
+              "plan": {
                   "slug": "basic",
                   "title": "Basic",
                   "description": "Basic Plan",
@@ -465,8 +466,9 @@ class CheckoutAPIView(CartMixin, OrganizationMixin,
                   "app_url":"/app/"
               },
               "auto_renew":true
-          },
-          "lines":[{
+            },
+            "lines": [
+            {
               "created_at":"2016-06-21T23:42:13.863739Z",
               "description":"Subscription to basic until 2016/11/21 (1 month)",
               "amount":"$20.00",
@@ -479,12 +481,11 @@ class CheckoutAPIView(CartMixin, OrganizationMixin,
               "dest_organization":"xia",
               "dest_amount":2000,
               "dest_unit":"usd"
-          }],
-          "options":[]
-        }]
+            }],
+            "options":[]
+          }]
         }
     """
-    # XXX replace key `items` by `results` to match other serializers?
     serializer_class = OrganizationCartSerializer
 
     @swagger_auto_schema(request_body=CheckoutSerializer, responses={
@@ -511,6 +512,8 @@ class CheckoutAPIView(CartMixin, OrganizationMixin,
         .. code-block:: http
 
             POST /api/billing/xia/checkout HTTP/1.1
+
+        .. code-block:: json
 
             {
                 "remember_card": true,
@@ -541,7 +544,7 @@ of Xia",
 
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        serializer = self.get_serializer({'items': queryset})
+        serializer = self.get_serializer({'results': queryset})
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):#pylint:disable=unused-argument
@@ -573,5 +576,5 @@ of Xia",
                 result = ChargeSerializer(charge)
                 return Response(result.data, status=status.HTTP_200_OK)
         except ProcessorError as err:
-            return Response({"details": err}, status=status.HTTP_403_FORBIDDEN)
+            return Response({'detail': err}, status=status.HTTP_403_FORBIDDEN)
         return Response({}, status=status.HTTP_200_OK)

saas/api/charges.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, DjaoDjin inc.
+# Copyright (c) 2020, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -34,7 +34,7 @@ from rest_framework.response import Response
 from .serializers import (ChargeSerializer, EmailChargeReceiptSerializer,
     RefundChargeSerializer, ValidationErrorSerializer)
 from .. import signals
-from ..docs import OpenAPIResponse, swagger_auto_schema
+from ..docs import OpenAPIResponse, no_body, swagger_auto_schema
 from ..filters import DateRangeFilter, OrderingFilter, SearchFilter
 from ..models import Charge, InsufficientFunds
 from ..mixins import ChargeMixin, OrganizationMixin
@@ -332,6 +332,7 @@ class EmailChargeReceiptAPIView(RetrieveChargeMixin, GenericAPIView):
     """
     serializer_class = EmailChargeReceiptSerializer
 
+    @swagger_auto_schema(request_body=no_body)
     def post(self, request, *args, **kwargs): #pylint: disable=unused-argument
         self.object = self.get_object()
         signals.charge_updated.send(

saas/api/organizations.py
@@ -289,6 +289,14 @@ class OrganizationPictureAPIView(OrganizationMixin, CreateAPIView):
         .. code-block:: http
 
             POST /api/profile/xia/picture/ HTTP/1.1
+
+        responds
+
+        .. code-block:: json
+
+            {
+              "location": "https://cowork.net/picture.jpg"
+            }
     """
     parser_classes = (parsers.FormParser, parsers.MultiPartParser)
     serializer_class = UploadBlobSerializer
@@ -297,7 +305,7 @@ class OrganizationPictureAPIView(OrganizationMixin, CreateAPIView):
         #pylint:disable=unused-argument
         uploaded_file = request.data.get('file')
         if not uploaded_file:
-            return Response({'details': "no location or file specified."},
+            return Response({'detail': "no location or file specified."},
                 status=status.HTTP_400_BAD_REQUEST)
 
         # tentatively extract file extension.
@@ -385,6 +393,29 @@ class OrganizationListAPIView(OrganizationSmartListMixin,
               "full_name": "Xia Lee",
               "type": "personal"
             }
+
+        responds
+
+        .. code-block:: json
+
+            {
+              "slug": "xia",
+              "email": "xia@locahost.localdomain",
+              "full_name": "Xia Lee",
+              "printable_name": "Xia Lee",
+              "type": "personal",
+              "credentials": true,
+              "default_timezone": "America/Los_Angeles",
+              "phone": "",
+              "street_address": "",
+              "locality": "",
+              "region": "",
+              "postal_code": "",
+              "country": "US",
+              "is_bulk_buyer": false,
+              "extra": null
+            }
+
         """
         return self.create(request, *args, **kwargs)
 

commit 8a66642dc90d86b12226bf6f15ab6733cda961b5

fixes regression introduced in commit c2324556 (`read_only_fields`)

saas/api/coupons.py
@@ -23,39 +23,22 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from __future__ import unicode_literals
 
+from django.db import IntegrityError
 from django.utils.translation import ugettext_lazy as _
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import (ListCreateAPIView,
     RetrieveUpdateDestroyAPIView)
+from rest_framework.response import Response
 
+from .serializers import CouponSerializer, CouponCreateSerializer
 from ..filters import OrderingFilter, SearchFilter, DateRangeFilter
 from ..models import Coupon
 from ..mixins import CouponMixin, ProviderMixin
-from .serializers import EnumField, PlanRelatedField
+from ..utils import handle_uniq_error
 
 #pylint: disable=no-init
 #pylint: disable=old-style-class
-
-
-class CouponSerializer(serializers.ModelSerializer):
-
-    discount_type = EnumField(choices=Coupon.DISCOUNT_CHOICES,
-        help_text=_("Type of discount (percentage or currency unit)"))
-    plan = PlanRelatedField(required=False, allow_null=True)
-
-    def validate_plan(self, plan):
-        if plan and not plan.is_active:
-            raise ValidationError(_("The plan is inactive. "\
-                "As a result the coupon will have no effect."))
-        return plan
-
-    class Meta:
-        model = Coupon
-        fields = ('code', 'discount_type', 'discount_value',
-            'created_at', 'ends_at', 'description',
-            'nb_attempts', 'plan')
-        read_only_fields = ('code',)
 
 
 class SmartCouponListMixin(object):
@@ -148,6 +131,8 @@ class CouponListCreateAPIView(SmartCouponListMixin, CouponQuerysetMixin,
         Customers will be able to use the `code` until `ends_at`
         to subscribe to plans from the Coupon's provider at a discount.
 
+        **Tags**: billing
+
         **Examples**
 
         .. code-block:: http
@@ -178,9 +163,20 @@ class CouponListCreateAPIView(SmartCouponListMixin, CouponQuerysetMixin,
         """
         return self.create(request, *args, **kwargs)
 
-    def perform_create(self, serializer):
-        serializer.save(organization=self.organization)
-
+    def create(self, request, *args, **kwargs):
+        serializer = CouponCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # check plan belongs to organization
+        plan = serializer.validated_data.get('plan')
+        if plan and plan.organization != self.organization:
+            raise ValidationError(
+                _("The plan does not belong to the organization."))
+        try:
+            serializer.save(organization=self.organization)
+        except IntegrityError as err:
+            handle_uniq_error(err)
+        headers = self.get_success_headers(serializer.data)
+        return Response(self.get_serializer().to_representation(serializer.instance), status=status.HTTP_201_CREATED, headers=headers)
 
 
 class CouponDetailAPIView(CouponMixin, RetrieveUpdateDestroyAPIView):

saas/api/serializers.py
@@ -53,8 +53,8 @@ from ..compat import reverse, six
 from ..decorators import _valid_manager
 from ..humanize import as_money
 from ..mixins import as_html_description, product_url
-from ..models import (AdvanceDiscount, BalanceLine, CartItem, Charge, Plan,
-    RoleDescription, Subscription, Transaction)
+from ..models import (AdvanceDiscount, BalanceLine, CartItem, Charge, Coupon,
+    Plan, RoleDescription, Subscription, Transaction)
 from ..utils import (build_absolute_uri, get_organization_model, get_role_model)
 
 #pylint: disable=no-init
@@ -202,6 +202,37 @@ class ChargeSerializer(serializers.ModelSerializer):
         model = Charge
         fields = ('created_at', 'amount', 'unit', 'readable_amount',
                   'description', 'last4', 'exp_date', 'processor_key', 'state')
+
+
+class CouponCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer to create a coupon, including the `code`.
+    """
+    discount_type = EnumField(choices=Coupon.DISCOUNT_CHOICES,
+        help_text=_("Type of discount (percentage or currency unit)"))
+    plan = PlanRelatedField(required=False, allow_null=True)
+
+    def validate_plan(self, plan):
+        if plan and not plan.is_active:
+            raise ValidationError(_("The plan is inactive. "\
+                "As a result the coupon will have no effect."))
+        return plan
+
+    class Meta:
+        model = Coupon
+        fields = ('code', 'discount_type', 'discount_value',
+            'created_at', 'ends_at', 'description',
+            'nb_attempts', 'plan')
+
+
+class CouponSerializer(CouponCreateSerializer):
+    """
+    Serializer to retrieve or update a `Coupon`.
+    """
+    class Meta:
+        model = CouponCreateSerializer.Meta.model
+        fields = CouponCreateSerializer.Meta.fields
+        read_only_fields = ('code',)
 
 
 class EmailChargeReceiptSerializer(NoModelSerializer):

saas/utils.py
@@ -286,35 +286,42 @@ def handle_uniq_error(err, renames=None):
     """
     Will raise a ``ValidationError`` with the appropriate error message.
     """
-    field_name = None
+    field_names = None
     err_msg = str(err).splitlines().pop()
     # PostgreSQL unique constraint.
     look = re.match(
-        r'DETAIL:\s+Key \(([a-z_]+)\)=\(.*\) already exists\.', err_msg)
+        r'DETAIL:\s+Key \(([a-z_]+(, [a-z_]+)*)\)=\(.*\) already exists\.',
+        err_msg)
     if look:
-        field_name = look.group(1)
+        field_names = [field_name for field_name in look.group(1).split(',')]
     else:
         look = re.match(
           r'DETAIL:\s+Key \(lower\(([a-z_]+)::text\)\)=\(.*\) already exists\.',
             err_msg)
         if look:
-            field_name = look.group(1)
+            field_names = [look.group(1)]
         else:
             # SQLite unique constraint.
-            look = re.match(
-                r'UNIQUE constraint failed: [a-z_]+\.([a-z_]+)', err_msg)
+            look = re.match(r'UNIQUE constraint failed: '\
+                '([a-z_]+\.[a-z_]+(, [a-z_]+\.[a-z_]+)*)', err_msg)
             if look:
-                field_name = look.group(1)
+                field_names = [field_name.split('.')[-1]
+                    for field_name in look.group(1).split(',')]
             else:
                 # On CentOS 7, installed sqlite 3.7.17
                 # returns differently-formatted error message.
                 look = re.match(
                     r'column ([a-z_]+) is not unique', err_msg)
                 if look:
-                    field_name = look.group(1)
-    if field_name:
-        if renames and field_name in renames:
-            field_name = renames[field_name]
+                    field_names = [look.group(1)]
+    if field_names:
+        renamed_fields = []
+        for field_name in field_names:
+            if renames and field_name in renames:
+                field_name = renames[field_name]
+            renamed_fields += [field_name]
+        # XXX retrieves `saas_coupon.code` duplicates
+        field_name = renamed_fields[-1]
         raise ValidationError({field_name:
             _("This %(field)s is already taken.") % {'field': field_name}})
     raise err