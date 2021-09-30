tekmickey0118/djaodjin-saas

commit c24555b096378b6ab08a045a2422ad418dfca462

updates to generate API doc correctly

saas/settings.py
@@ -120,6 +120,8 @@ _SETTINGS = {
     },
     'PROCESSOR_BACKEND_CALLABLE': None,
     'ROLE_RELATION': 'saas.Role',
+    'ROLE_SERIALIZER': 'saas.api.serializers.RoleSerializer',
+    'USER_SERIALIZER': 'saas.api.serializers.UserSerializer',
     'SEARCH_FIELDS_PARAM': 'q_f',
     'TERMS_OF_USE': 'terms-of-use',
 }
@@ -157,6 +159,8 @@ PROCESSOR_ID = PROCESSOR.get('INSTANCE_PK', 1)
 PROCESSOR_HOOK_URL = PROCESSOR.get('WEBHOOK_URL', 'stripe/postevent')
 PROCESSOR_HOOK_SECRET = PROCESSOR.get('WEBHOOK_SECRET')
 ROLE_RELATION = _SETTINGS.get('ROLE_RELATION')
+ROLE_SERIALIZER = _SETTINGS.get('ROLE_SERIALIZER')
+USER_SERIALIZER = _SETTINGS.get('USER_SERIALIZER')
 SEARCH_FIELDS_PARAM = _SETTINGS.get('SEARCH_FIELDS_PARAM')
 TERMS_OF_USE = _SETTINGS.get('TERMS_OF_USE')
 

saas/api/accounts.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, DjaoDjin inc.
+# Copyright (c) 2021, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,10 +26,11 @@ from django.contrib.auth import get_user_model
 from rest_framework.generics import ListAPIView
 
 from .organizations import OrganizationQuerysetMixin
-from .serializers import (OrganizationSerializer, UserSerializer)
+from .serializers import OrganizationSerializer
 from .. import filters
 from ..mixins import (OrganizationSmartListMixin, UserSmartListMixin)
 from ..pagination import TypeaheadPagination
+from ..utils import get_user_serializer
 
 
 #pylint: disable=no-init
@@ -60,29 +61,36 @@ def get_order_func(fields):
         get_order_func(fields[1:])(left, right))
 
 
-class AccountsSearchAPIView(OrganizationSmartListMixin,
+class AccountsTypeaheadAPIView(OrganizationSmartListMixin,
                             OrganizationQuerysetMixin, ListAPIView):
     """
-    Searches billing and login profiles
+    Searches profile and user accounts
 
-    Queries a page (``PAGE_SIZE`` records) of organization and user profiles.
+    Returns a list of {{MAX_TYPEAHEAD_CANDIDATES}} candidate profiles
+    or user accounts based of a search criteria (``q``).
 
-    The queryset can be filtered for at least one field to match a search
-    term (``q``).
+    The API is designed to be used in typeahead input fields. As such
+    it only returns results when the number of candidates is less
+    than {{MAX_TYPEAHEAD_CANDIDATES}}.
 
-    The queryset can be ordered by a field by adding an HTTP query parameter
-    ``o=`` followed by the field name. A sequence of fields can be used
-    to create a complete ordering by adding a sequence of ``o`` HTTP query
-    parameters. To reverse the natural order of a field, prefix the field
-    name by a minus (-) sign.
+    If you need to list all profiles, please see
+    `Lists billing profiles </docs/api/#listOrganization>`_
 
-    **Tags**: profile
+    The queryset can be further refined by a range of dates
+    ([``start_at``, ``ends_at``]), and sorted on specific fields (``o``).
+
+    The API is typically used in pages for the support team to quickly
+    locate an account. For example, it is used within the HTML
+    `provider dashboard page </docs/themes/#dashboard_metrics_dashboard>`_
+    as present in the default theme.
+
+    **Tags**: profile, user
 
     **Examples**
 
     .. code-block:: http
 
-        GET /api/accounts/?o=created_at HTTP/1.1
+        GET /api/accounts/?q=xi HTTP/1.1
 
     responds
 
@@ -90,8 +98,6 @@ class AccountsSearchAPIView(OrganizationSmartListMixin,
 
         {
             "count": 1,
-            "next": null,
-            "previous": null,
             "results": [{
                 "slug": "xia",
                 "full_name": "Xia Lee",
@@ -103,6 +109,7 @@ class AccountsSearchAPIView(OrganizationSmartListMixin,
     """
     serializer_class = OrganizationSerializer
     user_model = get_user_model()
+    pagination_class = TypeaheadPagination
 
     def get_users_queryset(self):
         # All users not already picked up as an Organization.
@@ -189,27 +196,32 @@ class AccountsSearchAPIView(OrganizationSmartListMixin,
 class ProfilesTypeaheadAPIView(OrganizationSmartListMixin,
                             OrganizationQuerysetMixin, ListAPIView):
     """
-    Searches billing profiles
+    Searches profiles
 
-    Queries a page (``PAGE_SIZE`` records) of candidate profiles based
-    of a search criteria (``q``).
+    Returns a list of {{MAX_TYPEAHEAD_CANDIDATES}} candidate profiles
+    based of a search criteria (``q``).
 
-    This API differs from /api/profile in that it is designed to be used
-    in auto-completion input fields instead of designed to list all profiles.
+    The API is designed to be used in typeahead input fields. As such
+    it only returns results when the number of candidates is less
+    than {{MAX_TYPEAHEAD_CANDIDATES}}.
 
-    The queryset can be ordered by a field by adding an HTTP query parameter
-    ``o=`` followed by the field name. A sequence of fields can be used
-    to create a complete ordering by adding a sequence of ``o`` HTTP query
-    parameters. To reverse the natural order of a field, prefix the field
-    name by a minus (-) sign.
+    If you need to list all profiles, please see
+    `Lists billing profiles </docs/api/#listOrganization>`_
 
-    **Tags**: profile
+    The queryset can be further refined by a range of dates
+    ([``start_at``, ``ends_at``]), and sorted on specific fields (``o``).
+
+    The API is typically used within an HTML
+    `connected profiles page </docs/themes/#dashboard_users_roles>`_
+    as present in the default theme.
+
+    **Tags**: profile, user
 
     **Examples**
 
     .. code-block:: http
 
-        GET /api/accounts/profiles/?q=xia HTTP/1.1
+        GET /api/accounts/profiles/?q=xi HTTP/1.1
 
     responds
 
@@ -217,8 +229,6 @@ class ProfilesTypeaheadAPIView(OrganizationSmartListMixin,
 
         {
             "count": 1,
-            "next": null,
-            "previous": null,
             "results": [{
                 "slug": "xia",
                 "full_name": "Xia Lee",
@@ -250,24 +260,32 @@ class UserQuerysetMixin(object):
 class UsersTypeaheadAPIView(UserSmartListMixin, UserQuerysetMixin,
                             ListAPIView):
     """
-    Searches login profiles
+    Searches users
 
-    Queries a page (``PAGE_SIZE`` records) of ``User``.
+    Returns a list of {{MAX_TYPEAHEAD_CANDIDATES}} candidate users
+    based of a search criteria (``q``).
 
-    The queryset can be filtered to a range of dates
-    ([``start_at``, ``ends_at``]) and for at least one field to match a search
-    term (``q``).
+    The API is designed to be used in typeahead input fields. As such
+    it only returns results when the number of candidates is less
+    than {{MAX_TYPEAHEAD_CANDIDATES}}.
 
-    Query results can be ordered by natural fields (``o``) in either ascending
-    or descending order (``ot``).
+    If you need to list all users, please see
+    `Lists user accounts </docs/api/#listUserListCreate>`_
 
-    **Tags**: profile
+    The queryset can be further refined by a range of dates
+    ([``start_at``, ``ends_at``]), and sorted on specific fields (``o``).
+
+    The API is typically used within an HTML
+    `profile role page </docs/themes/#dashboard_profile_roles>`_
+    as present in the default theme.
+
+    **Tags**: profile, user
 
     **Examples**
 
     .. code-block:: http
 
-        GET  /api/accounts/users/?o=created_at&ot=desc HTTP/1.1
+        GET  /api/accounts/users/?q=ali HTTP/1.1
 
     responds
 
@@ -275,17 +293,17 @@ class UsersTypeaheadAPIView(UserSmartListMixin, UserQuerysetMixin,
 
         {
             "count": 1,
-            "next": null,
-            "previous": null,
             "results": [
                 {
                     "slug": "alice",
+                    "created_at": "2014-01-01T00:00:00Z",
                     "email": "alice@djaodjin.com",
                     "full_name": "Alice Cooper",
-                    "created_at": "2014-01-01T00:00:00Z"
+                    "printable_name": "Alice Cooper",
+                    "username": "alice"
                 }
             ]
         }
     """
-    serializer_class = UserSerializer
+    serializer_class = get_user_serializer()
     pagination_class = TypeaheadPagination

saas/api/backend.py
@@ -32,7 +32,7 @@ from rest_framework.generics import (RetrieveAPIView,
 from rest_framework.response import Response
 
 from ..backends import ProcessorError
-from ..docs import swagger_auto_schema, Parameter, IN_PATH
+from ..docs import OpenAPIResponse, swagger_auto_schema, Parameter, IN_PATH
 from ..mixins import OrganizationMixin
 from ..models import get_broker
 from .serializers import (BankSerializer, CardSerializer,
@@ -44,15 +44,16 @@ class RetrieveBankAPIView(OrganizationMixin, RetrieveAPIView):
     """
     Retrieves a payout account
 
-    Pass through that calls the processor API to retrieve some details about
-    the deposit account associated to a provider (if that information is
+    Pass through that calls the payment processor API to retrieve some details
+    about the deposit account associated to a provider (if that information is
     available through the :doc:`payment processor backend<backends>` API).
 
     This API does not trigger payment of a subscriber to a provider. Checkout
     of a subscription cart is done either through the
-    :ref:`HTML page<pages_cart>` or :ref:`API end point<api_checkout>`.
+    :ref:`HTML page <pages_cart>` or
+    :ref:`API end point <api_checkout>`.
 
-    **Tags**: billing
+    **Tags**: billing, provider, profilemodel
 
     **Examples**
 
@@ -84,7 +85,7 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
     """
     Retrieves a payment method
 
-    Pass through to the processor to retrieve some details about
+    Pass through to the payment processor to retrieve some details about
     the payment method (ex: credit card) associated to a subscriber.
 
     When you wish to update the payment method on file through
@@ -93,13 +94,17 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
     in the processor.STRIPE_INTENT_SECRET field when the API is called
     with `?update=1` query parameters.
 
-    **Tags**: billing
+    The API is typically used within an HTML
+    `update payment method page </docs/themes/#dashboard_billing_card>`_
+    as present in the default theme.
+
+    **Tags**: billing, subscriber, profilemodel
 
     **Examples**
 
     .. code-block:: http
 
-        GET /api/billing/cowork/card/ HTTP/1.1
+        GET /api/billing/xia/card/ HTTP/1.1
 
     responds
 
@@ -112,20 +117,29 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
     """
     serializer_class = CardSerializer
 
+    def get_serializer_class(self):
+        if self.request.method.lower() in ('put',):
+            return CardTokenSerializer
+        return super(PaymentMethodDetailAPIView, self).get_serializer_class()
+
     def delete(self, request, *args, **kwargs):
         """
         Deletes a payment method
 
-        Pass through to the processor to remove the payment method (ex: credit
-        card) associated to a subscriber.
+        Pass through to the payment processor to remove the payment method
+        (ex: credit card) associated to a subscriber.
 
-        **Tags**: billing
+        The API is typically used within an HTML
+        `update payment method page </docs/themes/#dashboard_billing_card>`_
+        as present in the default theme.
+
+        **Tags**: billing, subscriber, profilemodel
 
         **Examples**
 
         .. code-block:: http
 
-            DELETE /api/billing/cowork/card/ HTTP/1.1
+            DELETE /api/billing/xia/card/ HTTP/1.1
         """
         return super(PaymentMethodDetailAPIView, self).delete(
             request, *args, **kwargs)
@@ -138,21 +152,27 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
         return super(PaymentMethodDetailAPIView, self).get(
             request, *args, **kwargs)
 
-    @swagger_auto_schema(request_body=CardTokenSerializer)
+    @swagger_auto_schema(responses={
+        200: OpenAPIResponse("Update successful", CardSerializer),
+    })
     def put(self, request, *args, **kwargs):
         """
         Updates a payment method
 
-        Pass through to the processor to update some details about
+        Pass through to the payment processor to update some details about
         the payment method (ex: credit card) associated to a subscriber.
 
-        **Tags**: billing
+        The API is typically used within an HTML
+        `update payment method page </docs/themes/#dashboard_billing_card>`_
+        as present in the default theme.
+
+        **Tags**: billing, subscriber, profilemodel
 
         **Examples**
 
         .. code-block:: http
 
-            PUT /api/billing/cowork/card/ HTTP/1.1
+            PUT /api/billing/xia/card/ HTTP/1.1
 
         .. code-block:: json
 
@@ -196,9 +216,10 @@ class PaymentMethodDetailAPIView(OrganizationMixin,
     def update(self, request, *args, **kwargs):
         #pylint:disable=unused-argument
         partial = kwargs.pop('partial', False)
-        serializer = CardTokenSerializer(data=request.data, partial=partial)
+        serializer = self.get_serializer_class()(
+            data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-        token = serializer.validated_data['token']
+        token = serializer.validated_data.get('token')
         try:
             new_card = self.organization.update_card(token, self.request.user)
         except ProcessorError as err:

saas/api/balances.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, DjaoDjin inc.
+# Copyright (c) 2021, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -24,8 +24,8 @@
 
 #pylint:disable=useless-super-delegation
 
-from django.db.models import F, Q, Max
 from django.db import transaction
+from django.db.models import F, Q, Max
 from rest_framework.generics import (get_object_or_404,
     GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
@@ -36,7 +36,8 @@ from ..metrics.base import abs_monthly_balances, monthly_balances
 from ..models import BalanceLine
 from ..filters import DateRangeFilter
 from ..mixins import DateRangeContextMixin
-from .serializers import BalanceLineSerializer, MetricsSerializer
+from .serializers import (BalanceLineSerializer, MetricsSerializer,
+    UpdateRankSerializer)
 
 #pylint: disable=no-init
 
@@ -48,7 +49,7 @@ class BrokerBalancesAPIView(DateRangeContextMixin, GenericAPIView):
 
     To add lines in the report see `/api/metrics/balances/{report}/lines/`.
 
-    **Tags**: metrics
+    **Tags**: metrics, broker, transactionmodel
 
     **Examples**
 
@@ -112,7 +113,7 @@ class BalanceLineListAPIView(ListCreateAPIView):
 
     Queries the list of rows reported on a balance sheet named `{report}`.
 
-    **Tags**: metrics
+    **Tags**: metrics, broker, transactionmodel
 
     **Examples**
 
@@ -140,6 +141,11 @@ class BalanceLineListAPIView(ListCreateAPIView):
     serializer_class = BalanceLineSerializer
     queryset = BalanceLine.objects.all()
 
+    def get_serializer_class(self):
+        if self.request.method.lower() in ('patch',):
+            return UpdateRankSerializer
+        return super(BalanceLineListAPIView, self).get_serializer_class()
+
     def get_queryset(self):
         return self.queryset.filter(
             report=self.kwargs.get('report')).order_by('rank')
@@ -157,7 +163,7 @@ class BalanceLineListAPIView(ListCreateAPIView):
 
         Adds a new row on the ``{report}`` balance sheet.
 
-        **Tags**: metrics
+        **Tags**: metrics, broker, transactionmodel
 
         **Examples**
 
@@ -187,7 +193,7 @@ class BalanceLineListAPIView(ListCreateAPIView):
             request, *args, **kwargs)
 
     @swagger_auto_schema(responses={
-        200: OpenAPIResponse("success", BalanceLineSerializer)})
+        200: OpenAPIResponse("success", BalanceLineSerializer(many=True))})
     def patch(self, request, *args, **kwargs):
         """
         Updates the order in which lines are displayed
@@ -196,7 +202,7 @@ class BalanceLineListAPIView(ListCreateAPIView):
         it will move the line at position 3 to position 1, updating the
         rank of all lines in-between.
 
-        **Tags**: metrics
+        **Tags**: metrics, broker, transactionmodel
 
         **Examples**
 
@@ -255,7 +261,7 @@ class BalanceLineDetailAPIView(RetrieveUpdateDestroyAPIView):
 
     Describes a row reported on a balance sheet named `{report}`.
 
-    **Tags**: metrics
+    **Tags**: metrics, broker, transactionmodel
 
     **Examples**
 
@@ -274,6 +280,10 @@ class BalanceLineDetailAPIView(RetrieveUpdateDestroyAPIView):
         }
     """
     serializer_class = BalanceLineSerializer
+    queryset = BalanceLine.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(report=self.kwargs.get('report'))
 
     def put(self, request, *args, **kwargs):
         """
@@ -281,7 +291,7 @@ class BalanceLineDetailAPIView(RetrieveUpdateDestroyAPIView):
 
         Updates a row reported on a balance sheet named `{report}`.
 
-        **Tags**: metrics
+        **Tags**: metrics, broker, transactionmodel
 
         **Examples**
 
@@ -316,7 +326,7 @@ class BalanceLineDetailAPIView(RetrieveUpdateDestroyAPIView):
 
         Deletes a row reported on a balance sheet named `{report}`.
 
-        **Tags**: metrics
+        **Tags**: metrics, broker, transactionmodel
 
         **Examples**
 
@@ -324,10 +334,9 @@ class BalanceLineDetailAPIView(RetrieveUpdateDestroyAPIView):
 
             DELETE /api/metrics/balances/taxes/lines/1/ HTTP/1.1
         """
-        return super(BalanceLineDetailAPIView, self).put(
+        return super(BalanceLineDetailAPIView, self).delete(
             request, *args, **kwargs)
 
     def get_object(self):
         return get_object_or_404(self.get_queryset(),
-            report=self.kwargs.get('report'),
             rank=self.kwargs.get('rank'))

commit 5cf4898b91a218907ec047b45fd43f4a3a345d90

splits serializers to override to avoid import loops
(issue was introduced in commit c24555b0)

saas/api/serializers.py
@@ -1098,36 +1098,6 @@ class ValidationErrorSerializer(NoModelSerializer):
         " the error in plain text"))
 
 
-# Default implementation when not overriden
-
-class UserSerializer(serializers.ModelSerializer):
-
-    # Only way I found out to remove the ``UniqueValidator``. We are not
-    # interested to create new instances here.
-    slug = serializers.CharField(source='username', validators=[
-        validators.RegexValidator(r'^[\w.@+-]+$', _('Enter a valid username.'),
-            'invalid')],
-        help_text=_("Effectively the username. The variable is named `slug`"\
-            " such that front-end code can be re-used between Organization"\
-            " and User records."))
-    email = serializers.EmailField(read_only=True,
-        help_text=_("E-mail address for the user"))
-    created_at = serializers.DateTimeField(source='date_joined', required=False,
-        help_text=_("Date/time of creation (in ISO format)"))
-    full_name = serializers.SerializerMethodField(
-        help_text=_("Full name for the contact (effectively first name"\
-        " followed by last name)"))
-
-    class Meta:
-        model = get_user_model()
-        fields = ('slug', 'email', 'full_name', 'created_at')
-        read_only = ('full_name', 'created_at',)
-
-    @staticmethod
-    def get_full_name(obj):
-        return obj.get_full_name()
-
-
 class OrganizationCartSerializer(NoModelSerializer):
     """
     Items which will be charged on an order checkout action.

saas/api/serializers_overrides.py
@@ -1,0 +1,61 @@
+# Copyright (c) 2021, DjaoDjin inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+Default implementation when not overriden
+"""
+
+from __future__ import unicode_literals
+
+from django.core import validators
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    # Only way I found out to remove the ``UniqueValidator``. We are not
+    # interested to create new instances here.
+    slug = serializers.CharField(source='username', validators=[
+        validators.RegexValidator(r'^[\w.@+-]+$', _('Enter a valid username.'),
+            'invalid')],
+        help_text=_("Effectively the username. The variable is named `slug`"\
+            " such that front-end code can be re-used between Organization"\
+            " and User records."))
+    email = serializers.EmailField(read_only=True,
+        help_text=_("E-mail address for the user"))
+    created_at = serializers.DateTimeField(source='date_joined', required=False,
+        help_text=_("Date/time of creation (in ISO format)"))
+    full_name = serializers.SerializerMethodField(
+        help_text=_("Full name for the contact (effectively first name"\
+        " followed by last name)"))
+
+    class Meta:
+        model = get_user_model()
+        fields = ('slug', 'email', 'full_name', 'created_at')
+        read_only = ('full_name', 'created_at',)
+
+    @staticmethod
+    def get_full_name(obj):
+        return obj.get_full_name()

saas/settings.py
@@ -121,7 +121,7 @@ _SETTINGS = {
     'PROCESSOR_BACKEND_CALLABLE': None,
     'ROLE_RELATION': 'saas.Role',
     'ROLE_SERIALIZER': 'saas.api.serializers.RoleSerializer',
-    'USER_SERIALIZER': 'saas.api.serializers.UserSerializer',
+    'USER_SERIALIZER': 'saas.api.serializers_overrides.UserSerializer',
     'SEARCH_FIELDS_PARAM': 'q_f',
     'TERMS_OF_USE': 'terms-of-use',
 }

testsite/settings.py
@@ -112,15 +112,15 @@ REST_FRAMEWORK = {
 MEDIA_ROOT = os.path.join(BASE_DIR, 'testsite', 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = ''
+APP_STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs', 'static')
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+if DEBUG:
+    STATIC_ROOT = ''
+    # Additional locations of static files
+    STATICFILES_DIRS = (APP_STATIC_ROOT,)
+else:
+    STATIC_ROOT = APP_STATIC_ROOT
 
 # List of finder classes that know how to find static files in
 # various locations.